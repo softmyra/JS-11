e68bc31eff3734abb80f6b5fdf8c44152de501a9
softmyra <rchaush7@gmail.com>
2022-07-09 23:41:41 +0530

    copied

---
+++ Javascript/FOR LOOP/index.js
@@ -0,0 +1,46 @@
+// Syntax
+// for(intialization;CSSConditionRule,increament/decreament){
+// statment1;
+// statment2;
+// statment3;
+// statment4;
+// }
+
+// Example
+// i=i+1(add karega ek bar + me)
+// console.log(i,"code will be executed if the given condition is true")
+// console.log(i,Math.trunc(Math.random() * 100));
+// console.log(i, "Your Code")
+//isme <= or >= operators bhi le sakte
+
+// Loop Within Loop
+// for(let exercise =1; exercise <=3; exercise++){
+//     console.log("Exercise",exercise);
+//     for (let rep =1; rep <= 10; rep++){
+//         console.log("rep==>",rep);
+// }
+        
+// }
+// OUTPUT
+// Exercise 1
+// Rep1
+// Rep2
+// ..
+// Rep5
+// Exercise 2
+// Rep1
+// Rep2
+// ..
+// Rep5
+// Exercise 3
+// Rep1
+// Rep2
+// ..
+// Rep5
+
+// Dynamic Value For Loop To be repeated
+
+var anynumber = 10;
+for(let i = 1;i <=anynumber;i++){
+    console.log("i", i);
+}
---
+++ Javascript/STRICT MODE/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// a = 20;
+// const interface=23;
+// const private = "exaltechs";
+
+let hasDriverslicence = false;
+const Passtest = true;
+if(Passtest){
+    hasDriverslicence = true;
+}
+
+console.log(hasDriverslicence);
+
+//(use strict):--> Must be at the top of the page.
+// used to strictly check the file.
+// we can not use verialble before intialization.
